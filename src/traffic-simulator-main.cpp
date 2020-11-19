0bd02da07c4aefabccfce58e7171c30290928926
Väinö Hatanpää <vaino.hatanpaa@aalto.fi>
2020-11-19 14:41:08 +0200

    Added Python3 to CMakeLists.txt and some sample plot to main function

--- src/traffic-simulator-main.cpp
+++ src/traffic-simulator-main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include "car.h"
+#include "matplotlibcpp.h"
+
+namespace plt = matplotlibcpp;
  int main() {
     std::cout << " I am traffic simulator" << std::endl;
+    plt::plot({1,3,2,4});
+    plt::show();
     return 0;
 }
 
